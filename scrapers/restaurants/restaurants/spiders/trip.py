cc8a6a01b526f64a5a2a194056c720696f123f2e
Lukasz Malucha <lucasmalucha@gmail.com>
2020-09-28 19:46:43 +0100

    data

--- scrapers/restaurants/restaurants/spiders/trip.py
+++ scrapers/restaurants/restaurants/spiders/trip.py
@@ -51,49 +51,59 @@ class TripSpider(scrapy.Spider):
 		sel = Selector(text=self.driver.page_source)
 		card=sel.xpath('.//div[@class="_1kXteagE"]')
 
-		titles=card.xpath('.//a[@class="_15_ydu6b"]/@href').extract()
-		link_list = []
-		for title in titles:
-			t = "https://www.tripadvisor.ie" + title
-			link_list.append(t)
+		for i in range(10):
+			next_button =  self.driver.find_element_by_xpath('//a[contains(@class, "nav next rndBtn ui_button primary taLnk")]')
+			next_button.click()
+			sleep(1)
+			"""
+			hrefy do listy z buttonu a pozniej loop
+			"""
 
-		self.driver.quit()	
 
-		for link in link_list:
-			self.driver = webdriver.Chrome(str(Path(Path.cwd(), "chromedriver.exe")), chrome_options=options)
-			self.driver.set_window_size(randrange(1100, 1200), randrange(800, 900))
-			self.driver.get(link)
-			sleep(3)
-			page = Selector(text=self.driver.page_source)
+
+		# titles=card.xpath('.//a[@class="_15_ydu6b"]/@href').extract()
+		# link_list = []
+		# for title in titles:
+		# 	t = "https://www.tripadvisor.ie" + title
+		# 	link_list.append(t)
+
+		# self.driver.quit()	
+
+		# for link in link_list:
+		# 	self.driver = webdriver.Chrome(str(Path(Path.cwd(), "chromedriver.exe")), chrome_options=options)
+		# 	self.driver.set_window_size(randrange(1100, 1200), randrange(800, 900))
+		# 	self.driver.get(link)
+		# 	sleep(2)
+		# 	page = Selector(text=self.driver.page_source)
 			
-			l = ItemLoader(item=RestaurantsItem(), selector=page)
+		# 	l = ItemLoader(item=RestaurantsItem(), selector=page)
 
-			title=page.xpath('.//h1[@class="_3a1XQ88S"]/text()').extract_first()
-			rating_string=page.xpath('.//span[@class="_3KcXyP0F wxkjkcMP _1BKcz_Ze"]/@title').extract_first()
-			if rating_string:
-				rating=rating_string[0]   !!  podziel po spacji 
-			else:
-				rating=3
-			cuisine_list=page.xpath('.//span[@class="_13OzAOXO _34GKdBMV"]/a/text()').extract()
-			if cuisine_list:
-				cuisine=(", ").join(cuisine_list[1:])
+		# 	title=page.xpath('.//h1[@class="_3a1XQ88S"]/text()').extract_first()
+		# 	rating_string=page.xpath('.//a[@class="_3S6pHEQs"]/span/@title').extract_first()
+		# 	if rating_string:
+		# 		rating=rating_string.split(" ")[0]  
+		# 	else:
+		# 		rating="3"
+		# 	cuisine_list=page.xpath('.//span[@class="_13OzAOXO _34GKdBMV"]/a/text()').extract()
+		# 	if cuisine_list:
+		# 		cuisine=(", ").join(cuisine_list[1:])
 
-			location=page.xpath('.//a[@class="_15QfMZ2L"]/text()').extract_first()
-			if location:
-				locality=location.split(", ")[-2]
-				address_list=location.split(", ")[:-2]
-				address=(", ").join(address_list)
+		# 	location=page.xpath('.//a[@class="_15QfMZ2L"]/text()').extract_first()
+		# 	if location:
+		# 		locality=location.split(", ")[-2]
+		# 		address_list=location.split(", ")[:-2]
+		# 		address=(", ").join(address_list)
 
 
-			l.add_value('title', title)
-			l.add_value('rating', rating)
-			l.add_value('cuisine', cuisine)
-			l.add_value('locality', locality)
-			l.add_value('address', address)
-			l.add_value('link', link)
-			yield l.load_item()	
+		# 	l.add_value('title', title)
+		# 	l.add_value('rating', rating)
+		# 	l.add_value('cuisine', cuisine)
+		# 	l.add_value('locality', locality)
+		# 	l.add_value('address', address)
+		# 	l.add_value('link', link)
+		# 	yield l.load_item()	
 
-			self.driver.quit()	
+		# 	self.driver.quit()	
 
 
 
