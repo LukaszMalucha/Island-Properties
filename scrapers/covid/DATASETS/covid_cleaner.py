9f4baad2124f9859004d31d942224e2892417284
Lukasz Malucha <lucasmalucha@gmail.com>
2021-02-28 15:39:04 +0000

    data

--- scrapers/covid/DATASETS/covid_cleaner.py
+++ scrapers/covid/DATASETS/covid_cleaner.py
@@ -19,7 +19,7 @@ Created on Sat Oct 10 19:57:34 2020
 #
 #dataset.to_csv("country_medical.csv", index = False)
 #
-###################################### https://www.ecdc.europa.eu/en/covid-19/data
+###################################### https://www.ecdc.europa.eu/en/publications-data/data-national-14-day-notification-rate-covid-19
 
 import pandas as pd
 import datetime
@@ -48,18 +48,34 @@ dataset_weekly = dataset_weekly_cases.merge(dataset_weekly_deaths, on=["country"
 
 dataset_weekly["country"] = dataset_weekly["country"].str.replace("_", " ")
 
+
+dataset_weekly["date"] = dataset_weekly["year_week"].apply(lambda x: datetime.datetime.strptime(x + '-1', "%Y-%W-%w"))
+dataset_weekly["date"] = dataset_weekly["date"].astype(str)        
+
+
+## REMOVE THAT EXTRA WEEK IN BETWEEN 
+dataset_weekly = dataset_weekly[dataset_weekly["year_week"] != "2020-53"  ]
+
+
+
+
+dataset_weekly = dataset_weekly.drop("year_week", axis=1)
+
+
+    
+
 dataset_weekly.to_csv("covid_weekly.csv", index = False)
 
 
-###################################### Testing Rates Both Datasets (positivty & testing )  https://ourworldindata.org/coronavirus-testing   
+###################################### Testing Rates Both Datasets (positivty & testing 1st and 3rd map)  https://ourworldindata.org/coronavirus-testing   
 
 
 dataset_testing = pd.read_csv("daily-tests-per-thousand-people-smoothed-7-day.csv", encoding="utf-8")
 dataset_testing = dataset_testing[["Entity", "Date","new_tests_per_thousand_7day_smoothed"]]
 dataset_testing = dataset_testing.rename(columns={"Entity": "country", "new_tests_per_thousand_7day_smoothed": "testing_rate", "Date": "date"})
-#dataset_testing = dataset_testing.sort_values(by=["Date"], ascending=False)
-#dataset_testing["d"] = dataset_testing["country"].duplicated(keep="first")
-#dataset_testing = dataset_testing[dataset_testing["d"] == False]
+dataset_testing = dataset_testing.sort_values(by=["date"], ascending=False)
+dataset_testing["d"] = dataset_testing["country"].duplicated(keep="first")
+dataset_testing = dataset_testing[dataset_testing["d"] == False]
 
 
 
@@ -68,14 +84,14 @@ dataset_testing = dataset_testing.rename(columns={"Entity": "country", "new_test
 dataset_positivity = pd.read_csv("positive-rate-daily-smoothed.csv", encoding="utf-8")
 dataset_positivity = dataset_positivity[["Entity", "Date","short_term_positivity_rate"]]
 dataset_positivity = dataset_positivity.rename(columns={"Entity": "country", "short_term_positivity_rate": "positivity_rate", "Date": "date"})
-#dataset_positivity = dataset_positivity.sort_values(by=["Date"], ascending=False)
-#dataset_positivity["d"] = dataset_positivity["country"].duplicated(keep="first")
-#dataset_positivity = dataset_positivity[dataset_positivity["d"] == False]
+dataset_positivity = dataset_positivity.sort_values(by=["date"], ascending=False)
+dataset_positivity["d"] = dataset_positivity["country"].duplicated(keep="first")
+dataset_positivity = dataset_positivity[dataset_positivity["d"] == False]
 
 
-dataset_testing = dataset_testing.merge(dataset_positivity, on=["country", "date"])
+dataset_testing = dataset_testing.merge(dataset_positivity, on=["country"])
 
-
+dataset_testing = dataset_testing[["country", "testing_rate","positivity_rate" ]]
 
 
 #dataset_testing = pd.read_csv("covid_testing_org.csv", encoding="utf-8")
@@ -88,7 +104,7 @@ dataset_testing = dataset_testing.merge(dataset_positivity, on=["country", "date
 #
 #dataset_testing_weeks = dataset_testing_weeks[["country", "testing_rate", "positivity_rate" ]]
 
-dataset_testing_weeks.to_csv("covid_testing.csv", index = False)
+dataset_testing.to_csv("covid_testing.csv", index = False)
 
 
 
@@ -100,6 +116,8 @@ from sklearn.preprocessing import MinMaxScaler
 def country_checker(string):
         if string in countries:
             return True
+        
+      
 
 dataset_1 = pd.read_csv("covid_weekly.csv", encoding="utf-8")
 dataset_2 = pd.read_csv("country_medical.csv", encoding="utf-8")
@@ -110,26 +128,33 @@ dataset_1["country"] = dataset_1["country"].str.replace("Czechia", "Czech Republ
 dataset_3["country"] = dataset_3["country"].str.replace("Czechia", "Czech Republic")
 dataset_4["country"] = dataset_4["country"].str.replace("Slovak Republic", "Slovakia")
 
-#dataset_2["check"]  = dataset_2["country"].apply(lambda x: country_checker(x))    
-#dataset_2 = dataset_2[dataset_2["check"] == True]
-#dataset_2 = dataset_2.drop("check", axis =1 )
-#dataset_2.to_csv("country_medical.csv", index=False)
+countries = list(dataset_2["country"].unique())  
+countries_3 = list(dataset_3["country"].unique())  
 
 dataset_final = dataset_1.merge(dataset_2, how="left", on=["country"]) 
 
+dataset_final["check"]  = dataset_final["country"].apply(lambda x: country_checker(x))    
+dataset_final = dataset_final[dataset_final["check"] == True]
+dataset_final = dataset_final.drop("check", axis =1 )
+
+
+
+
+
 dataset_final = dataset_final.merge(dataset_3, how="left", on=["country"]) 
+dataset_final = dataset_final[~dataset_final["testing_rate"].isnull()]
+
+
 
 dataset_final = dataset_final.merge(dataset_4, how="left", on=["country"]) 
 
 
 dataset_final = dataset_final.drop_duplicates()
 
-countries = list(dataset_3["country"].unique())
 
 
-dataset_final["date"] = dataset_final["year_week"].apply(lambda x: datetime.datetime.strptime(x + '-1', "%Y-%W-%w"))
-dataset_final["date"] = dataset_final["date"].astype(str)        
-    
+
+
 
 
 
@@ -150,7 +175,7 @@ lst = list(dataset_final.columns)
 scaler = MinMaxScaler()
 
 
-dataset_final["deaths_per_capita"] = round((dataset_final["deaths"] / dataset_final["Population"]), 4)
+dataset_final["deaths_per_capita"] = round((dataset_final["deaths"] / dataset_final["Population"]), 6)
 
 dataset_final[["cases_per_capita", 'median_age', 'gdp_per_capita', 'diabetes_prevalence', 'curative_beds_per_hundred_thousand', 'life_expectancy', 'testing_rate', 'positivity_rate', ]] = scaler.fit_transform(dataset_final[["cases_per_capita", 'median_age', 'gdp_per_capita', 'diabetes_prevalence', 'curative_beds_per_hundred_thousand', 'life_expectancy', 'testing_rate', 'positivity_rate']].values)
 
@@ -160,29 +185,6 @@ dataset_final["severity_ratio"] = (dataset_final["cases_per_capita"]  + (dataset
 
 
 
-
-
-
-
-
-
-
-
-## REMOVE THAT EXTRA WEEK IN BETWEEN 
-dataset_final = dataset_final[dataset_final["year_week"] != "2020-53"  ]
-
-
-
-
-dataset_final = dataset_final.drop("year_week", axis=1)
-
-
-
-
-#dataset_final["severity_ratio"] = dataset_final["severity_ratio"] * 1000
-#
-#dataset_final["deaths_per_capita"] = round((dataset_final["deaths"] / dataset_final["Population"]) * 1000, 4)
-    
 
 
 dataset_final.to_csv("dataset_covid.csv", index=False)
